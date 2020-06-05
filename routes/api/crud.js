9071b031418071b4eaf03f197e8c755a560fc969
GauravWalia19 <gauravwalia019@gmail.com>
2020-06-05 17:04:21 +0530

    added heroku deployment configurations

--- routes/api/crud.js
+++ routes/api/crud.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true});
 const db = mongoose.connection;
 db.on('error',console.error.bind(console, 'Connection error:'));
 
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 // exclusing dotenv config from production
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
+// CORS Middleware
+app.use(cors());
+
 // express middleware handling the body parsing 
 app.use(express.json());
 
 // express middleware handling the form parsing
 app.use(express.urlencoded({extended: false}));
+
+// middleware for handling sample api routes
+app.use('/api/v1', require('./routes/api/crud'));
 
 // create static assets from react code for production only
 if (process.env.NODE_ENV === 'production') {
@@ -20,9 +27,6 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
-
-// middleware for handling sample api routes
-app.use('/api/v1', require('./routes/api/crud'));
 
 // use port from environment variables for production
 const PORT = process.env.PORT || 5000;
