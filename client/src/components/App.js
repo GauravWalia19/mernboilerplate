fea3f1bc322c6ccd5cc2023acfc02f6ab1689b65
GauravWalia19 <gauravwalia019@gmail.com>
2020-10-31 15:56:18 +0530

    updated to react 17

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>client/src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,16 +16,22 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 17
         </a>
-        <br/>
+        <p>
+          Edit <code>routes/api/crud.js</code> for adding Backend APIs.<br/>
+          View existing APIs at <a className="App-link"
+          href="http://localhost:5000/api/v1/get"
+          target="_blank"
+          rel="noopener noreferrer">/api/v1/...</a>
+        </p>
         <a 
           className="App-link"
           href="https://github.com/GauravWalia19/MernBoilerPlate"
           target="_blank"
           rel="noopener noreferrer"
         >
-          MERN Boilerplate
+          MERN Boilerplate 1.0.0
         </a>
         <br />
         <a 
--- routes/api/crud.js
+++ routes/api/crud.js
@@ -1,37 +1,62 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true});
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 const db = mongoose.connection;
-db.on('error',console.error.bind(console, 'Connection error:'));
+db.on('error', console.error.bind(console, 'Connection error:'));
 
-// create
-router.post('/post', (req,res)=>{
+/**
+ * @method POST
+ * @access public
+ * @endpoint /api/v1/post
+ **/
+router.post('/post', (req, res) => {
     res.json({
-        message: "POST API for MERN Boilerplate"
-    })
-})
+        message: 'POST API for MERN Boilerplate',
+    });
+});
 
-// read
-router.get('/get', (req,res)=>{
+/**
+ * @method GET
+ * @access public
+ * @endpoint /api/v1/get
+ **/
+router.get('/get', (req, res) => {
     res.json({
-        message: "GET API for MERN Boilerplate"
-    })
-})
+        message: 'GET API for MERN Boilerplate',
+        APIs: 'Other Endpoints',
+        create: '/api/v1/post',
+        read: '/api/v1/get',
+        update: '/api/v1/put/<ID>',
+        delete: '/api/v1/delete/<ID>'
+    });
+});
 
-// update
-router.put('/put/:id',(req,res)=>{
+/**
+ * @method PUT
+ * @access public
+ * @endpoint /api/v1/put/32323
+ **/
+router.put('/put/:id', (req, res) => {
     res.json({
-        message: `PUT ${req.params.id} API for MERN Boilerplate`
-    })
-})
+        message: `PUT ${req.params.id} API for MERN Boilerplate`,
+    });
+});
 
-// delete
-router.delete('/delete/:id', (req,res)=>{
+/**
+ * @method DELETE
+ * @access public
+ * @endpoint /api/v1/delete/424
+ **/
+router.delete('/delete/:id', (req, res) => {
     res.json({
-        message: `DELETE ${req.params.id} API for MERN Boilerplate`
-    })
-})
+        message: `DELETE ${req.params.id} API for MERN Boilerplate`,
+    });
+});
 
 module.exports = router;
