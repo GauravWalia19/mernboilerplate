91fdf4c6f4b95c3d953c75af980670e4ebdb3896
memoriesdead <98496589+memoriesdead@users.noreply.github.com>
2022-12-25 15:40:50 -0800

    Update serviceWorker.js

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -1,3 +1,15 @@
+// This optional code is used to register a service worker.
+// register() is not called by default.
+
+// This lets the app load faster on subsequent visits in production, and gives
+// it offline capabilities. However, it also means that developers (and users)
+// will only see deployed updates on subsequent visits to a page, after all the
+// existing tabs open on the page have been closed, since previously cached
+// resources are updated in the background.
+
+// To learn more about the benefits of this model and instructions on how to
+// opt-in, read https://bit.ly/CRA-PWA
+
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -121,3 +133,4 @@ export function unregister() {
     });
   }
 }
+
