6d4fa41bd9f56eff59f98b4cf24644df252fb101
memoriesdead <98496589+memoriesdead@users.noreply.github.com>
2022-12-25 15:41:17 -0800

    Update setupTests.js

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,5 +1,33 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// jest-fetch-mock adds mock functions for testing API calls.
+import 'jest-fetch-mock';
+
+// jest-date-mock adds mock functions for testing time-based logic.
+import 'jest-date-mock';
+
+// Set up the global fetch mock.
+fetchMock.enableMocks();
+
+// Custom matcher for testing the content of a DOM element.
+expect.extend({
+  toHaveTextContentCaseInsensitive(received, expected) {
+    const receivedText = received.textContent.trim().toLowerCase();
+    const expectedText = expected.trim().toLowerCase();
+    const pass = receivedText === expectedText;
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to have text content matching "${expected}"`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to have text content matching "${expected}", but it was "${receivedText}"`,
+        pass: false,
+      };
+    }
+  },
+});
